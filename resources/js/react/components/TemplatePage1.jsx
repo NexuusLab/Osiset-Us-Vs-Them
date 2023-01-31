fd68bd8c067c8160bb892ee0f571b655ee66c737
Zain-Irfan009 <zain.irfan@tetralogicx.com>
2023-01-31 10:35:20 +0500

    commit

--- resources/js/react/components/TemplatePage1.jsx
+++ resources/js/react/components/TemplatePage1.jsx
@@ -206,10 +206,9 @@ export function TemplatePage1() {
                         onAction: handleSelectProducts
                     }}
                     description={`Choose the template that best suits your needs. You will then be able to fully customize it.`}>
-                    <img
-                        alt="table1"
-                        className='MediaCard-Img'
-                        src="https://i.ibb.co/QXcJW8V/image.png"
+                    <VideoThumbnail
+                        videoLength={80}
+                        thumbnailUrl="https://www.youtube.com/watch?v=DK7fcW4t3tg"
                     />
                 </MediaCard>
                 {toastProducts}
