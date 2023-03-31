61425f61b10029cb7f890d9c4ef623ace1eee3f9
Zain-Irfan009 <zain.irfan@tetralogicx.com>
2023-03-31 15:44:15 +0500

    commit

--- app/Jobs/AfterAuthenticateJob.php
+++ app/Jobs/AfterAuthenticateJob.php
@@ -36,44 +36,43 @@ class AfterAuthenticateJob implements ShouldQueue
      */
     public function handle(Request $request)
     {
+        if($request->input('shop')) {
+            $productController = new ProductController();
+            $productController->SyncProdcuts($request->shop);
+            $shop = User::where('name', $request->shop)->first();
+            $products = Product::where('shop_id', $shop->id)->get();
 
-       $productController = new ProductController();
-        $productController->SyncProdcuts($request->shop);
-        $shop=User::where('name',$request->shop)->first();
-        $products=Product::where('shop_id',$shop->id)->get();
-
-        foreach ($products as $product) {
-            $res = $shop->api()->rest('get', '/admin/products/' . $product->shopify_id . '/metafields.json');
+            foreach ($products as $product) {
+                $res = $shop->api()->rest('get', '/admin/products/' . $product->shopify_id . '/metafields.json');
 
                 foreach ($res['body']['container']['metafields'] as $deliverydate) {
                     if ($deliverydate['key'] == 'products') {
                         $delete = $shop->api()->rest('delete', '/admin/metafields/' . $deliverydate['id'] . '.json');
                     }
                 }
-        }
-        $get_metafield = $shop->api()->rest('get', '/admin/metafields.json');
-        foreach ($get_metafield['body']['container']['metafields'] as $meta) {
-            if ($meta['namespace'] == 'usvsthem') {
-                $delete = $shop->api()->rest('delete', '/admin/metafields/' . $meta['id'] . '.json');
             }
+            $get_metafield = $shop->api()->rest('get', '/admin/metafields.json');
+            foreach ($get_metafield['body']['container']['metafields'] as $meta) {
+                if ($meta['namespace'] == 'usvsthem') {
+                    $delete = $shop->api()->rest('delete', '/admin/metafields/' . $meta['id'] . '.json');
+                }
+            }
+            $shop_metafield = $shop->api()->rest('post', '/admin/metafields.json', [
+                "metafield" => array(
+                    "key" => 'setting',
+                    "value" => 1,
+                    "type" => "number_integer",
+                    "namespace" => "usvsthem"
+                )
+            ]);
+            if ($shop_metafield['errors'] == false) {
+
+                $shop->metafield_id = $shop_metafield['body']['metafield']['id'];
+                $shop->save();
+
+            }
+
         }
-        $shop_metafield = $shop->api()->rest('post','/admin/metafields.json', [
-            "metafield" => array(
-                "key" => 'setting',
-                "value" => 1,
-                "type" => "number_integer",
-                "namespace" => "usvsthem"
-            )
-        ]);
-        if($shop_metafield['errors']==false){
-
-            $shop->metafield_id=$shop_metafield['body']['metafield']['id'];
-            $shop->save();
-
-        }
-
-
-
 
     }
 }
